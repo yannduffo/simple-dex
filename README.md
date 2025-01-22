f7dd38a36f5a13274db72484732a522a07c196f4
yannduffo <yann.duffo@gmail.com>
2025-01-22 10:27:03 +0100

    DexSwap contract coding

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # simple-dex
+
 Simple Decentralized Exchange app built using Solidity Smart Contracts and a React front-end interface
+
+## Main features
+
+For now only simple features will be implemented :
+
+- Add/remove liquidity
+- Swap functions
---
+++ smart-contract/contracts/DexSwap.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./DexPool.sol";
+
+contract DexSwap {
+    //defining state variable
+    address[] public allPairs;
+    mapping(address => mapping(address => DexPool)) public getPair;
+
+    event PairCreated(
+        address indexed token1,
+        address indexed token2,
+        address pair
+    );
+
+    function createPairs(
+        address token1,
+        address token2,
+        string calldata token1Name,
+        string calldata token2Name
+    ) external returns (address) {
+        //checkings
+        require(token1 != token2, "Identical addresses are not allowed");
+        require(
+            address(getPair[token1][token2]) == address(0),
+            "Pair already exists"
+        );
+
+        //pool creation
+        DexPool dexPool = new DexPool();
+        getPair[token1][token2] = dexPool;
+        getPair[token2][token1] = dexPool; //pools are bidirectionals
+        allPairs.push(address(dexPool));
+
+        //emit pair creation event
+        emit PairCreated(token1, token2, address(dexPool));
+
+        return address(dexPool);
+    }
+
+    function allPairsLenght() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    //get the whole table (not only index selected one which is automatly created as "allParis" is Public)
+    function getPairs() external view returns (address[] memory) {
+        return allPairs;
+    }
+}
