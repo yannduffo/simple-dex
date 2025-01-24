25552db5ecd811206f76cd4dadd40cd37382ca11
yannduffo <yann.duffo@gmail.com>
2025-01-24 17:20:34 +0100

    copyAbis.js script optimisaiton and correction

--- frontend/copyAbis.js
+++ frontend/copyAbis.js
@@ -1,57 +1,49 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-//defining paths
-const sourceDir = path.resolve(__dirname, '../smart-contract/build/contracts');
-const tragetDir = path.resolve(__dirname, './src/abi');
+async function cleanAndCopy() {
+    //defining paths
+    const sourceDir = path.resolve(__dirname, '../smart-contract/build/contracts');
+    const tragetDir = path.resolve(__dirname, './src/abi');
 
-//check that the 2 directories exist
-if(!fs.existsSync(sourceDir)) {
-    console.error("The ABI files source dir does not exist", sourceDir);
-    process.exit(1);
-}
-if(!fs.existsSync(tragetDir)) {
-    console.error("The ABI files target dir does not exist", tragetDir);
-    process.exit(1);
-}
-
-//cleaning repository 
-fs.readdir(tragetDir, (err, file) => {
-    if(!err) {
-        file.forEach((file) => {
-            const filePath = path.join(tragetDir, file);
-            fs.unlink(filePath, (err) => {
-                if (err) console.error("Error while deleting ABI files", err);
-            });
-        });
-        console.log("All ABI files deleted");
+    //check that the 2 directories exist
+    if(!await fs.access(sourceDir).then(() => true).catch(() => false)) {
+        console.error("The ABI files source dir does not exist", sourceDir);
+        process.exit(1);
     }
-});
-
-//copying ABI files
-fs.readdir(sourceDir, (err, files) => {
-    //processing errors
-    if(err){
-        console.error("Error while charging ABI files", err);
+    if(!await fs.access(tragetDir).then(() => true).catch(() => false)) {
+        console.error("The ABI files target dir does not exist", tragetDir);
         process.exit(1);
     }
 
-    //copying each files
-    files.forEach((file) => {
-        if(file.endsWith('.json')) {
-            //defining full file name
-            const sourceFile = path.join(sourceDir, file);
-            const targetFile = path.join(tragetDir, file);
+    try {
+        //cleaning repository 
+        console.log("Cleaning target repository...");
+        const files = await fs.readdir(tragetDir);
+        await Promise.all(files.map(async (file) => {
+            const filePath = path.join(tragetDir, file);
+            await fs.unlink(filePath)
+        }));
+        console.log("All ABI files deleted");
 
-            //actually copying the file
-            fs.copyFile(sourceFile, targetFile, (err) => {
-                //processing err
-                if(err){
-                    console.error("Error while copying ABI file named : ", file, ": ", err);
-                } else {
-                    console.log("ABI file copied : ", file);
-                }
-            });
-        }
-    });
-});
+        //copying ABI files
+        console.log("Copying ABI files...");
+        const sourceFiles = await fs.readdir(sourceDir);
+        await Promise.all(sourceFiles.map(async (file) => {
+            if(file.endsWith('.json')){
+                const sourceFile = path.join(sourceDir, file);
+                const targetFile = path.join(tragetDir, file);
+                await fs.copyFile(sourceFile, targetFile);
+                console.log("ABI file copied : ", file);
+            }
+        }));
+        console.log("All ABI files copied");
+
+    } catch (err) {
+        console.error("An error occurded while cleaning or copying", err);
+        process.exit(1);
+    }
+}
+
+//calling func
+cleanAndCopy();
