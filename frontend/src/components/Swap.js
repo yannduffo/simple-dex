c0a2f817039d0048a2e31ecc690f1bbd95d34629
yannduffo <yann.duffo@gmail.com>
2025-01-24 17:49:43 +0100

    swap almost working each time, problem with fee applicaiton

--- frontend/src/components/Swap.js
+++ frontend/src/components/Swap.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { dexPool } from '../utils/contracts';
+import { dexPool, tokenABCContract } from '../utils/contracts';
+import web3 from '../utils/web3';
 
 const Swap = () => {
     //state variables 
@@ -14,15 +15,21 @@ const Swap = () => {
         const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
         const account = accounts[0];
 
-        //getting expectedAmountOut from the SC
-        const expectedAmountOut = await dexPool.methods.getExpectedAmountOut(tokenA, tokenB, amountIn).call();
-        setAmountOut(expectedAmountOut);
+        //converting amountIn to wei values
+        const amountInInWei = web3.utils.toWei(amountIn.toString(), "ether");
 
-        console.log("amountIn : ", amountIn);
+        //getting expectedAmountOut from the SC
+        const expectedAmountOut = await dexPool.methods.getExpectedAmountOut(tokenA, tokenB, amountInInWei).call();
+        setAmountOut(expectedAmountOut); //the value retrun already is in WEI
+
+        //approve DexPool contract to spend amountIn of TokenA
+        await tokenABCContract.methods.approve(dexPool.options.address, amountInInWei).send({from: account});
+
+        console.log("amountInWei : ", amountInInWei);
         console.log("amountOut : ", amountOut);
 
         //swaping
-        await dexPool.methods.swapTokens(tokenA, tokenB, amountIn, amountOut).send({from: account});
+        await dexPool.methods.swapTokens(tokenA, tokenB, amountInInWei, amountOut).send({from: account});
         alert("Swap successfuly done");
     }
 
@@ -31,7 +38,7 @@ const Swap = () => {
             <h1>Swap</h1>
             <input placeholder="Token A" value={tokenA} onChange={(e) => setTokenA(e.target.value)} />
             <input placeholder="Token B" value={tokenB} onChange={(e) => setTokenB(e.target.value)} />
-            <input placeholder="Amount" value={amountIn} onChange={(e) => setAmountIn(e.target.value)} />
+            <input type="number" placeholder="Amount" value={amountIn} onChange={(e) => setAmountIn(e.target.value)} />
             <button onClick={handleSwap}>Swap</button>
         </div>
     );
